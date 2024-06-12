eaaf01550d637fc4eaff68d5371cac5c167a5332
Juan Garcia <jgarcia@tramisalud.com>
2024-06-12 01:01:50 -0500

    Anadir todoReduce al repo

---
+++ useTodo/todoReducer.js
@@ -0,0 +1,22 @@
+export const todoReducer = (initialState = [], action) => {
+
+    switch (action.type) {
+        case 'add':
+            return [...initialState, action.payload];
+        case 'delete':
+            return initialState.filter(todo => todo.id!== action.payload);     
+        case 'update':
+            return initialState.map(todo => {
+                if(todo.id === action.payload ){
+                    return {
+                       ...todo,
+                        done:!todo.done
+                    }
+                }
+                return todo;
+            });
+        
+        default:
+            return initialState;
+    }
+}
